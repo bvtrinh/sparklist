8ce32262e961bf3b1ed42f5dcb01ea69edb9e854
Amandeep Rehal <arehal@sfu.ca>
2020-03-02 21:51:04 -0800

    refactored local user registration

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 require('dotenv').config();
 
 const saltRounds = 10;
@@ -7,67 +8,70 @@ const saltRounds = 10;
 // User Model
 const User = require('../models/User');
 
+// handle registeration
+router.post('/register', (req, res) => {
+    const {fname, lname, email, password, passwordConfirm} = req.body;
+    let errors = [];
 
-function userExists(req, res, next){
-    User.find({email: req.body.email}, function(err, docs) {
-		if (docs.length){
-            // user already exists, display error
-            res.redirect('pages/register', {failed: true});
-		}else{
-            // user with entered email does not exist
-            // create user
-            next(req, res);
-        }
-    });
-}
+    // check required fields
+    if(!fname || !lname || !email || !password || !passwordConfirm){
+        errors.push({msg: 'Please fill in all fields'});
+    }
 
-function addUser(req, res) {
-    bcrypt.hash(req.body.password, saltRounds, function (err,   hash) {
-        var user = {
-            fname: req.body.fname,
-            lname: req.body.lname,
-            email: req.body.email,
-            password: hash
-        };
+    // Check passwords match
+    if(password !== passwordConfirm) {
+        errors.push({msg: 'Passwords do not match'});
+    }
 
-        var newUser = new User(user);
+    // Check password length
+    if(password.length < 6) {
+        errors.push({msg: 'Password should be atleast 6 characters long.'});
+    }
 
-        newUser.save()
-        .then(item => {
-            // should render homepage or myaccount page once implemented
-            res.send("item saved to database");
-        })
-        .catch(err => {
-            res.status(400).send("unable to save to database");
+    if(errors.length > 0) {
+        res.render('pages/register', {
+            errors,
+            fname,
+            lname,
+            email,
+            password,
+            passwordConfirm
         });
-    });
-}
-
-router.post('/create', (req, res) => {
-    userExists(req, res, addUser);
-});
-
-router.post('/auth', (req, res) => {
-    User.findOne({email: req.body.email}, function(err, user) {
-		if (user){
-            // user exists
-            bcrypt.compare(req.body.password, user.password, function(err, result) {
-                if(result){
-                    var loginMsg = "Successfully logged in as " + req.body.email;
-                    res.redirect('/');
+    } else {
+        // validation passed
+        // check if user exists, create new user if doesnt exist
+        User.findOne({email: email})
+            .then(user => {
+                if(user){
+                    // user exists, redirect to register page
+                    errors.push({msg: 'User with entered email already exists. Please try again.'});
+                    res.render('pages/register', {
+                        errors,
+                        fname,
+                        lname,
+                        email,
+                        password,
+                        passwordConfirm
+                    });
                 } else {
-                    res.render('pages/login', {failed: true});
-                }
+                    bcrypt.hash(req.body.password, saltRounds, function (err,   hash) {
+                        new User ({
+                            fname: fname,
+                            lname: lname,
+                            email: email,
+                            password: hash
+                        }).save().then((newUser) => {
+                            // res.send("item saved to database");
+                            req.flash('success_msg', 'You are now registered, please login to continue.');
+                            res.redirect('/login');
+                        })
+                        .catch(err => {
+                            res.status(400).send("unable to save to database");
+                        }); 
+                    });
+                } 
             });
-        }else{
-            // user does not exist
-            res.render('pages/login', {failed: true});
-
-        }
-    });
+    }
 });
-
-
-
 
 module.exports = router;
