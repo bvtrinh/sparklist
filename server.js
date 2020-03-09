d0eab2fcadae3b1cd8965fa1ed9cd5ccc3798e66
Tyler Trinh <tylervtrinh@gmail.com>
2020-03-09 00:48:17 -0700

    Update .env path for production

--- server.js
+++ server.js
@@ -1,64 +1,65 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 const sendEmail = require("./scripts/email/index");
-require('dotenv').config();
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 // Routes
-const items = require('./routes/item');
-const users = require('./routes/user');
+const items = require("./routes/item");
+const users = require("./routes/user");
 
 const app = express();
 
 // Body parser middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // DB Config
 const db = process.env.MONGOURI;
 
 // Connect to Mongo
 mongoose
-	.connect(db, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-	})
-	.then(() => console.log('MongoDB Connected...'))
-	.catch((err) => console.log(err));
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
 
 // Use routes
-app.use('/item', items);
-app.use('/user', users);
+app.use("/item", items);
+app.use("/user", users);
 
-app.get('/', (req, res) => {
-	const sendObj = {
-		msg: 'Hello World!',
-	};
+app.get("/", (req, res) => {
+  const sendObj = {
+    msg: "Hello World!"
+  };
 
-	res.render('pages/home', sendObj);
+  res.render("pages/home", sendObj);
 });
 
-app.get('/email', async (req, res) => {
+app.get("/email", async (req, res) => {
+  const toEmail = "tylervtrinh@gmail.com";
+  const params = {
+    email: toEmail,
+    name: "Tyler Trinh"
+  };
 
-	const toEmail = "tylervtrinh@gmail.com";
-	const params = {
-		email: toEmail,
-		name: "Tyler Trinh"
-	};
+  const result = await sendEmail({
+    template: "email",
+    params: params,
+    email: toEmail,
+    subject: "Testing 123"
+  });
 
-	const result = await sendEmail({template:"email", 
-		params: params, 
-		email: toEmail, 
-		subject: "Testing 123"});
-
-	return res.send(result);
+  return res.send(result);
 });
 
-console.log('Running on ' + process.env.NODE_ENV);
+console.log("Running on " + process.env.NODE_ENV);
 
 const port = process.env.PORT || 5000;
 
