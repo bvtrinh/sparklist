e4738c7268e76dca95bb0c51b37be11d3915b596
Tyler Trinh <tylervtrinh@gmail.com>
2020-03-09 23:10:33 -0700

    Add search bar and page

--- routes/item.js
+++ routes/item.js
@@ -1,21 +1,27 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // Item Model
-const Item = require('../models/Item');
+const Item = require("../models/Item");
 
-router.get('/', (req, res) => {
-	Item.find().sort({ date: -1 }).then((items) => res.json(items));
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json(items));
 });
 
 // @route POST api/items
 // @desc Create an item
 // @access Private
-router.post('/', (req, res) => {
-	const new_item = new Item({
-		name: req.body.name,
-	});
+router.post("/", (req, res) => {
+  const new_item = new Item({
+    name: req.body.name
+  });
 
-	new_item.save().then((item) => res.json(item));
+  new_item.save().then(item => res.json(item));
+});
+
+router.get("/search", (req, res) => {
+  res.render("pages/search");
 });
 
 module.exports = router;
