08084fa6683996ba0f531cd8f2951479b0ad09f1
Tyler Trinh <tylervtrinh@gmail.com>
2020-03-10 19:54:15 -0700

    Change login route for protected route

--- routes/profile.js
+++ routes/profile.js
@@ -1,20 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 const authCheck = (req, res, next) => {
-    if(!req.user){
-        // user not logged in
-        res.redirect('/login');
-    } else {
-        // user logged in
-        next();
-    }
-}
+  if (!req.user) {
+    // user not logged in
+    res.redirect("/user/login");
+  } else {
+    // user logged in
+    next();
+  }
+};
 
-router.get('/', authCheck, (req, res) => {
-    // var username = req.user.fname + ' ' + req.user.lname;
-    res.render('pages/profile', {user: req.user});
-    // res.render('pages/login', {failed: true});
-
-})
+router.get("/", authCheck, (req, res) => {
+  // var username = req.user.fname + ' ' + req.user.lname;
+  res.render("pages/profile", { user: req.user });
+  // res.render('pages/login', {failed: true});
+});
 
 module.exports = router;
