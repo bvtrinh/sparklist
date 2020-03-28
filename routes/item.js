24934300c77d7519100c05cb12ecc95b9ea44790
Amandeep Rehal <arehal@sfu.ca>
2020-03-27 18:23:09 -0700

    added functions for scraping amazon and google shopping

--- routes/item.js
+++ routes/item.js
@@ -144,6 +144,97 @@ router.get("/find", (req, res) => {
   res.render("pages/item/findItem");
 });
 
+async function scrapeAmazon ( url ) {
+  return new Promise(resolve => {
+    priceFind.findItemDetails(url, async (err, itemDetails) => {
+      // console.log("itemDetails: " + itemDetails)
+      const img_url = await scrape.amazon(url);
+      const labels = await labeler(img_url[0]);
+  
+      var item = new Item({
+        title: itemDetails.name,
+        price_hist: { price: itemDetails.price, date: Date().toString() },
+        current_price: itemDetails.price,
+        category: itemDetails.category,
+        img_url: img_url[0],
+        url,
+        labels
+      });
+      resolve(item);
+    });
+  });
+}
+
+
+async function scrapeGoogleShop ( itemName ) {
+  try {
+    // const itemName = req.body.itemName;
+
+    // open the headless browser
+    var browser = await puppeteer.launch({ headless: true });
+
+    // open a new page
+    var page = await browser.newPage();
+
+    // enter url in page
+    await page.goto(`https://shopping.google.com/`);
+    await page.type('input.lst', itemName);
+    page.keyboard.press('Enter');
+    await page.waitForSelector('a.VZTCjd.REX1ub.translate-content');
+  
+    // scrape info from page
+    var itemLinks = await page.evaluate(() => {
+
+      var links = document.querySelectorAll(`a.VZTCjd.REX1ub.translate-content`);
+
+      let itemLinks = [];
+      var j = 0;
+      for (var i = 0; i < links.length; i++) {
+        if(links[i].getAttribute("href").includes("shopping")){
+          itemLinks[j] = "https://www.google.com" + links[i].getAttribute("href");
+          j++;
+        }
+      }
+
+      return itemLinks;
+    });
+
+    let items = [];
+    for (var i = 0; i < itemLinks.length; i++) {
+
+      await page.goto(itemLinks[i]);
+      await page.waitForSelector('span.BvQan.sh-t__title-pdp.sh-t__title.translate-content');
+
+      items[i] = await page.evaluate((i) => {
+        item = {
+          title: document.querySelector(`span.BvQan.sh-t__title-pdp.sh-t__title.translate-content`).innerHTML.trim(),
+          current_price: document.querySelector(`span.NVfoXb > b`).innerHTML.trim().replace('$',''),
+          url: "google.com" + document.querySelector(`a.txYPsb.mpeCOc.shntl`).getAttribute("href"),
+          img_url:  document.querySelector(`img.sh-div__image.sh-div__current`).getAttribute("src")
+        }
+
+        return item;
+      });
+    }
+    
+    for (var i = 0; i < items.length; i++) {
+      // get actual link to item
+      await page.goto("https://www." + items[i].url);
+      items[i].url = await page.url();
+    }
+
+    await browser.close();
+    console.log(success("Browser Closed"));
+
+    return items;
+
+  } catch (err) {
+    // Catch and display errors
+    console.log(error(err));
+    await browser.close();
+    console.log(error("Browser Closed"));
+  }
+}
 router.post("/scrape", async (req, res) => {
 
   try {
