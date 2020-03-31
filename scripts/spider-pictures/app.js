417bb47ab87d176b98ac49a54efa3df66f0649ee
Tyler Trinh <tylervtrinh@gmail.com>
2020-03-31 00:18:35 -0700

    Add another tag for amazon imgs

--- scripts/spider-pictures/app.js
+++ scripts/spider-pictures/app.js
@@ -2,7 +2,7 @@ const app = require("./index.js");
 
 async function test() {
   const urls = await app.amazon(
-    "https://www.amazon.ca/Eufy-BoostIQ-Super-Thin-Self-Charging-Medium-Pile/dp/B079QYYGF1?pf_rd_r=2F3PFW6P6F6DB8BWYT0P&pf_rd_p=71d5c146-52b1-46d7-887b-f7929473a113&pd_rd_r=5f0b1c0e-dcf5-4deb-8f7a-5efbcd6c9579&pd_rd_w=O9KCu&pd_rd_wg=zugfT&ref_=pd_gw_hl-stp-home"
+    "https://www.amazon.ca/Subtle-Art-Not-Giving-Counterintuitive/dp/0062641549?ref_=Oct_TopRatedC_916520_3&pf_rd_r=7AG37VTD9F0D2ND4H9RP&pf_rd_p=6c478dbf-4b91-52d8-8eb2-d2598ba547c0&pf_rd_s=merchandised-search-11&pf_rd_t=101&pf_rd_i=916520&pf_rd_m=A3DWYIK6Y9EEQB"
   );
   console.log(urls[0]);
 }
