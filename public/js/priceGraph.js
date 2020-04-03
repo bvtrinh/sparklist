561228c8058368c68ffb42c360fd06bfe7247b90
Tyler Trinh <tylervtrinh@gmail.com>
2020-04-02 17:45:07 -0700

    Move price graphing to separate js file

---
+++ public/js/priceGraph.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+  const id = $("#itemID").val();
+
+  $.ajax({
+    url: "/item/priceHistory",
+    method: "POST",
+    data: { itemID: id },
+    dataType: "JSON",
+    success: function(data) {
+      graph(data);
+    }
+  });
+});
+
+var itemInfo;
+
+function graph(data) {
+  itemInfo = data.results;
+  google.charts.load("current", { packages: ["corechart"] });
+  google.charts.setOnLoadCallback(drawBasic);
+}
+
+function prepData(graphData) {
+  var allData = [];
+
+  for (var i = 0; i < graphData.length; i++) {
+    var currentData = [];
+
+    var date = new Date(graphData[i].date);
+    var dateString = date.getMonth() + "/" + date.getDate();
+
+    currentData.push(dateString);
+    currentData.push(graphData[i].price);
+    allData.push(currentData);
+  }
+
+  return allData;
+}
+
+function dataMinMax(graphData) {
+  var prices = [];
+  for (var i = 0; i < graphData.length; i++) {
+    prices[i] = graphData[i].price;
+  }
+
+  var min = Math.ceil(Math.min.apply(null, prices)) - 5;
+  var max = Math.ceil(Math.max.apply(null, prices)) + 5;
+
+  return { min: min, max: max };
+}
+
+function drawBasic() {
+  price_hist = itemInfo.price_hist;
+  graphData = prepData(price_hist);
+
+  var data = new google.visualization.DataTable();
+  data.addColumn("string", "Date");
+  data.addColumn("number", "Price");
+  data.addRows(graphData);
+
+  var winMinMax = dataMinMax(price_hist);
+
+  var options = {
+    vAxis: {
+      format: "currency",
+      gridlines: {
+        count: 10
+      },
+      viewWindowMode: "explicit",
+      viewWindow: {
+        max: winMinMax.max,
+        min: winMinMax.min
+      }
+    },
+    legend: {
+      position: "none"
+    },
+    series: {
+      0: {
+        type: "steppedArea",
+        areaOpacity: 0
+      }
+    }
+  };
+
+  var chart = new google.visualization.SteppedAreaChart(
+    document.getElementById("chart_div")
+  );
+
+  chart.draw(data, options);
+}
