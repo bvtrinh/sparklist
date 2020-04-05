7162c9cb97a2baa9c6ee43246f98df93812663ef
Tyler Trinh <tylervtrinh@gmail.com>
2020-04-05 16:45:39 -0700

    Add user agent header to axios

--- scripts/spider-pictures/index.js
+++ scripts/spider-pictures/index.js
@@ -1,39 +1,49 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const amazon = async url => {
+const amazon = async (url) => {
   const page = await getPage(url);
   const imageSrc = await getImageSrc(page);
   return imageSrc;
 };
 
-const getTitle = async url => {
+const getTitle = async (url) => {
   const page = await getPage(url);
   const $ = cheerio.load(page.res.data);
   var title;
-  title = $("h1")
-    .first()
-    .text();
+  title = $("h1").first().text();
   // Amazon places the title in a span within the h1
   if (title === "") {
-    title = $("h1")
-      .children()
-      .first()
-      .text();
+    title = $("h1").children().first().text();
   }
   if (title === "") return "Title was not found";
   return title.trim();
 };
 
-const getPage = async url => {
+const getPage = async (url) => {
   try {
-    return { res: await axios.get(url) };
+    return {
+      res: await axios.get(url, {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36",
+        },
+      }),
+    };
   } catch (err) {
-    return { res: await axios.get(url) };
+    console.log(err);
+    return {
+      res: await axios.get(url, {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36",
+        },
+      }),
+    };
   }
 };
 
-const getImageSrc = async page => {
+const getImageSrc = async (page) => {
   let $ = cheerio.load(page.res.data);
   let imageSrcArr = [];
   $(".imgTagWrapper")
@@ -61,5 +71,5 @@ const getImageSrc = async page => {
 
 module.exports = {
   amazon,
-  getTitle
+  getTitle,
 };
